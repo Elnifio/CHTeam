b9a3b8febd04d7116ef92166cb4b7958185e3990
Zhaojie Chen <31903753+zc700015@users.noreply.github.com>
2022-03-14 21:31:34 -0400

    Update models.py

--- Minizon/MiniAmazon/models.py
+++ Minizon/MiniAmazon/models.py
@@ -55,5 +55,3 @@ class Order(db.Model):
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
     seller_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
-    def __repr__(self):
-        return f'<Order {self.name}>'
