b712510b04b70a94fd36271890b751a46c768671
Elnifio <elnifio0519@gmail.com>
2022-04-10 22:51:07 -0400

    Update example.js

--- AutomatedTest/example.js
+++ AutomatedTest/example.js
@@ -1,10 +1,34 @@
 const puppeteer = require("puppeteer");
 
+const username = "TestUser04";
+const password = "123456";
+const address = "None";
+const email = `${username}@email.com`;
+
 (async() => {
+    // Launch the browser
     const browser = await puppeteer.launch();
-    console.log(await browser.version());
+
+    // Connects to the page
     const page = await browser.newPage();
-    await page.goto("https://127.0.0.1:5000");
+    await page.goto("http://127.0.0.1:5000/");
+
+    // registers the account
+    await page.click("a.btn.btn-sm");
+    await page.type("#username", username);
+    await page.type("#email", email);
+    await page.type("#address", address);
+    await page.type("#password1", password);
+    await page.type("#password2", password);
+    
+    // 
+    const [response] = await Promise.all([
+        page.click("#submit"),
+        page.waitForNavigation({waitUntil: "networkidle2"}),
+        
+      ]);
+
+    console.log(response);
     await page.pdf({path: "page.pdf", format: "A4"});
     await browser.close();
 })();
